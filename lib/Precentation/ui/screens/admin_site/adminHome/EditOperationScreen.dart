69cd3d8a423c90c8b9bdbca3fc7651a293ba32f8
saria <tasnim.saria33@gmail.com>
2024-02-17 15:20:12 +0600

    nothing

--- lib/Precentation/ui/screens/admin_site/adminHome/EditOperationScreen.dart
+++ lib/Precentation/ui/screens/admin_site/adminHome/EditOperationScreen.dart
@@ -1,28 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:frastraited/Precentation/ui/utility/app_colors.dart';
-import 'package:frastraited/screen/onboarding/loginScreen.dart';
 import 'package:frastraited/screen/widgets/bodyBackground.dart';
 
 class EditOperation extends StatefulWidget {
-  const EditOperation({super.key});
+  const EditOperation({Key? key}) : super(key: key);
 
   @override
   State<EditOperation> createState() => _EditOperationState();
 }
 
 class _EditOperationState extends State<EditOperation> {
-  final List<Map<String, dynamic>> availableVaccines = [
+  // Sample list of available operation packages
+  final List<Map<String, dynamic>> availableOperationPackages = [
     {
-      'name': 'Hepatitis B Vaccine',
-      'description': 'Protects against hepatitis B virus',
-      'imageUrl': 'https://example.com/hepatitis_b_vaccine.jpg',
+      'name': 'Heart Surgery',
+      'description': 'Heart operation package',
+      'imageUrl': 'https://example.com/heart_surgery.jpg',
+      'amount': 'BDT 10000',
     },
     {
-      'name': 'HPV Vaccine',
-      'description': 'Protects against human papillomavirus',
-      'imageUrl': 'https://example.com/hpv_vaccine.jpg',
+      'name': 'Brain Surgery',
+      'description': 'Brain operation package',
+      'imageUrl': 'https://example.com/brain_surgery.jpg',
+      'amount': 'BDT 15000',
     },
-    // Add more vaccine information here
+    {
+      'name': 'Knee Replacement',
+      'description': 'Knee operation package',
+      'imageUrl': 'https://example.com/knee_replacement.jpg',
+      'amount': 'BDT 8000',
+    },
+    // Add more operation package information here
   ];
 
   @override
@@ -49,32 +57,32 @@ class _EditOperationState extends State<EditOperation> {
                     ],
                   ),
                   const SizedBox(
-                    height: 40,
+                    height: 10,
                   ),
                   Text(
-                    'Available Vaccines',
+                    'Available Operation Packages',
                     style: TextStyle(
                       fontSize: 24,
                       fontWeight: FontWeight.bold,
                       color: AppColors.primaryColor,
                     ),
                   ),
-                  const SizedBox(height: 30),
+                  const SizedBox(height: 40),
                   ListView.builder(
                     shrinkWrap: true,
                     physics: NeverScrollableScrollPhysics(),
-                    itemCount: availableVaccines.length,
+                    itemCount: availableOperationPackages.length,
                     itemBuilder: (BuildContext context, int index) {
-                      final vaccine = availableVaccines[index];
+                      final package = availableOperationPackages[index];
                       return Column(
                         children: [
                           Card(
+                            color: Colors.white,
+                            elevation: 3,// Common color for all cards
                             borderOnForeground: true,
-                            color: Colors.white, // Common color for all cards
-                            elevation: 3,
                             child: Container(
                               height: 120, // Adjust the height of the container
-                              padding: const EdgeInsets.all(8),
+                              //padding: const EdgeInsets.all(8),
                               child: Row(
                                 children: [
                                   Container(
@@ -83,7 +91,7 @@ class _EditOperationState extends State<EditOperation> {
                                     decoration: BoxDecoration(
                                       borderRadius: BorderRadius.circular(8),
                                       image: DecorationImage(
-                                        image: NetworkImage(vaccine['imageUrl']),
+                                        image: NetworkImage(package['imageUrl']),
                                         fit: BoxFit.cover,
                                       ),
                                     ),
@@ -95,16 +103,38 @@ class _EditOperationState extends State<EditOperation> {
                                       mainAxisAlignment: MainAxisAlignment.center,
                                       children: [
                                         Text(
-                                          vaccine['name'],
+                                          package['name'],
                                           style: TextStyle(fontSize: 18, color: AppColors.primaryColor),
                                         ),
                                         const SizedBox(height: 4),
                                         Text(
-                                          vaccine['description'],
+                                          package['description'],
                                           style: TextStyle(color: Colors.grey),
+                                        ),
+                                        const SizedBox(height: 4),
+                                        Text(
+                                          'Amount: ${package['amount']}',
+                                          style: TextStyle(fontWeight: FontWeight.bold,color: Colors.blueGrey),
                                         ),
                                       ],
                                     ),
+                                  ),
+                                  Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      IconButton(
+                                        icon: Icon(Icons.delete),
+                                        onPressed: () {
+                                          _showDeleteAlertDialog(context, index);
+                                        },
+                                      ),
+                                      IconButton(
+                                        icon: Icon(Icons.edit),
+                                        onPressed: () {
+                                          _showEditBottomSheet(context, package, index);
+                                        },
+                                      ),
+                                    ],
                                   ),
                                 ],
                               ),
@@ -120,6 +150,176 @@ class _EditOperationState extends State<EditOperation> {
           ),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _showAddBottomSheet(context);
+        },
+        child: Icon(Icons.add),
+        backgroundColor: AppColors.primaryColor,
+      ),
+    );
+  }
+
+  void _showDeleteAlertDialog(BuildContext context, int index) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Delete Operation Package?"),
+          content: Text("Do you want to delete the Package from the list?"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  availableOperationPackages.removeAt(index);
+                });
+                Navigator.of(context).pop();
+              },
+              child: Text(
+                "Delete",
+                style: TextStyle(color: Colors.white),
+              ),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(AppColors.primaryColor),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("Cancel"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showEditBottomSheet(BuildContext context, Map<String, dynamic> package, int index) {
+    TextEditingController nameController = TextEditingController(text: package['name']);
+    TextEditingController descriptionController = TextEditingController(text: package['description']);
+    TextEditingController amountController = TextEditingController(text: package['amount']);
+    TextEditingController imageUrlController = TextEditingController(text: package['imageUrl']);
+
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      builder: (context) {
+        return SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.only(
+              bottom: MediaQuery.of(context).viewInsets.bottom,
+            ),
+            child: Container(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextField(
+                    controller: nameController,
+                    decoration: InputDecoration(labelText: 'Name'),
+                  ),
+                  SizedBox(height: 16),
+                  TextField(
+                    controller: descriptionController,
+                    decoration: InputDecoration(labelText: 'Description'),
+                  ),
+                  SizedBox(height: 16),
+                  TextField(
+                    controller: amountController,
+                    decoration: InputDecoration(labelText: 'Amount'),
+                  ),
+                  SizedBox(height: 16),
+                  TextField(
+                    controller: imageUrlController,
+                    decoration: InputDecoration(labelText: 'Image URL'),
+                  ),
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        availableOperationPackages[index]['name'] = nameController.text;
+                        availableOperationPackages[index]['description'] = descriptionController.text;
+                        availableOperationPackages[index]['amount'] = amountController.text;
+                        availableOperationPackages[index]['imageUrl'] = imageUrlController.text;
+                      });
+                      Navigator.pop(context);
+                    },
+                    child: Text('Update',
+                      style: TextStyle(color: Colors.white),),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  void _showAddBottomSheet(BuildContext context) {
+    TextEditingController nameController = TextEditingController();
+    TextEditingController descriptionController = TextEditingController();
+    TextEditingController amountController = TextEditingController();
+    TextEditingController imageUrlController = TextEditingController();
+
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      builder: (context) {
+        return SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.only(
+              bottom: MediaQuery.of(context).viewInsets.bottom,
+            ),
+            child: Container(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextField(
+                    controller: nameController,
+                    decoration: InputDecoration(labelText: 'Name'),
+                  ),
+                  SizedBox(height: 16),
+                  TextField(
+                    controller: descriptionController,
+                    decoration: InputDecoration(labelText: 'Description'),
+                  ),
+                  SizedBox(height: 16),
+                  TextField(
+                    controller: amountController,
+                    decoration: InputDecoration(labelText: 'Amount'),
+                  ),
+                  SizedBox(height: 16),
+                  TextField(
+                    controller: imageUrlController,
+                    decoration: InputDecoration(labelText: 'Image URL'),
+                  ),
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        availableOperationPackages.add({
+                          'name': nameController.text,
+                          'description': descriptionController.text,
+                          'amount': amountController.text,
+                          'imageUrl': imageUrlController.text,
+                        });
+                      });
+                      Navigator.pop(context);
+                    },
+                    child: Text('Add',
+                      style: TextStyle(color: Colors.white),),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
