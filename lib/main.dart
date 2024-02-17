a0a8717ab7e434150f8f0854e1d8e989acc9e63a
saria <tasnim.saria33@gmail.com>
2024-02-17 09:58:04 +0600

    nothing

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,11 @@ import 'package:frastraited/firebase_options.dart';
 import 'package:frastraited/screen/onboarding/forgotPasswordScreen.dart';
 import 'package:frastraited/screen/onboarding/loginScreen.dart';
 import 'package:frastraited/screen/onboarding/pinVerificationScreen.dart';
-import 'package:frastraited/screen/onboarding/registrationScreen.dart';
 import 'package:frastraited/screen/onboarding/resetPasswordScreen.dart';
 import 'package:frastraited/screen/onboarding/signUpScreen.dart';
 import 'package:frastraited/screen/onboarding/splashScreen.dart';
 
 void main() async {
-  ///
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
@@ -31,7 +29,6 @@ class MyApp extends StatelessWidget {
         '/': (context) => const splashScreen(),
         '/login': (context) => const loginScreen(),
         '/pinVarification': (context) => const pinVerificationScreen(),
-        '/registration': (context) => const registrationScreen(),
         '/setPassword': (context) => const resetPasswordScreen(),
         '/signUp': (context) => const signUpScreen(),
         '/forgotPassword': (context) => const forgotPasswordScreen(),
