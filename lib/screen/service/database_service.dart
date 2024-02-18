bd33f9b97f0ad61f2aa47b985bfd9706a0b6522a
S.M. SHAHi <s.m.kamalhussain@gmail.com>
2024-02-19 00:02:27 +0600

    pending tests done

--- lib/screen/service/database_service.dart
+++ lib/screen/service/database_service.dart
@@ -1,14 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:frastraited/screen/service/models/doctors.dart';
 import 'package:frastraited/screen/service/models/operationPackages.dart';
+import 'package:frastraited/screen/service/models/pending_test_model.dart';
 import 'package:frastraited/screen/service/models/users.dart';
 import 'package:frastraited/screen/service/models/vaccines.dart';
 
 class DatabaseTables {
   static const users = "users";
   static const doctors = "doctors";
-  static const vaccines="vaccines";
-  static const operationPackages="operationPackages";
+  static const vaccines = "vaccines";
+  static const operationPackages = "operationPackages";
+  static const pendingTests = "pendingTests";
+  static const pendingTestsUser = "pendingTestsUser";
 }
 
 class DatabaseService {
@@ -102,7 +105,7 @@ class DatabaseService {
     return vaccineList;
   }
 
- //For Operation
+  //For Operation
 
   Future<void> setOperationInformation(OperationModel model) async {
     CollectionReference result = fireStore.collection(DatabaseTables.operationPackages);
@@ -115,7 +118,6 @@ class DatabaseService {
     CollectionReference result = fireStore.collection(DatabaseTables.operationPackages);
     result.doc(model.id).delete();
   }
-
 
   Future<void> updateOperationInformation(OperationModel model) async {
     CollectionReference result = fireStore.collection(DatabaseTables.operationPackages);
@@ -137,6 +139,32 @@ class DatabaseService {
     return operationList;
   }
 
+  Future<List<PendingTestModel>> getPendingTest() async {
+    List<PendingTestModel> pendingTestList = [];
 
+    await fireStore.collection(DatabaseTables.pendingTests).get().then((QuerySnapshot querySnapshot) {
+      pendingTestList.clear();
+      for (var doc in querySnapshot.docs) {
+        Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+        PendingTestModel pendingTest = PendingTestModel.fromJson(data);
+        pendingTestList.add(pendingTest);
+      }
+    });
 
+    return pendingTestList;
+  }
+
+  Future<void> setPendingTest(PendingTestModel model) async {
+    CollectionReference result = fireStore.collection(DatabaseTables.pendingTestsUser);
+    final pendingTestId = result.doc().id;
+
+    result.doc(pendingTestId).set(model.copyWith(id: pendingTestId).toJson());
+  }
+
+  Future<void> updatePendingTestUserMedicalId(PendingTestModel model) async {
+    CollectionReference result = fireStore.collection(DatabaseTables.pendingTestsUser);
+    final pendingTestId = result.doc().id;
+
+    result.doc(pendingTestId).update(model.toJson());
+  }
 }
