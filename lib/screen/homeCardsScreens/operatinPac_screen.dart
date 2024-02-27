b33530fb8bc2221b3b89200ba542378414c46491
S.M. SHAHi <s.m.kamalhussain@gmail.com>
2024-02-27 11:39:59 +0600

    operation package update user

--- lib/screen/homeCardsScreens/operatinPac_screen.dart
+++ lib/screen/homeCardsScreens/operatinPac_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:frastraited/Precentation/ui/utility/app_colors.dart';
 import 'package:frastraited/Precentation/ui/utility/search_field.dart'; // Import the SearchField widget
+import 'package:frastraited/Precentation/ui/widgets/empty_container_view.dart';
 import 'package:frastraited/screen/service/database_service.dart';
 import 'package:frastraited/screen/service/models/operationPackages.dart';
 import 'package:frastraited/screen/widgets/bodyBackground.dart';
@@ -13,10 +14,12 @@ class OperationScreen extends StatefulWidget {
 }
 
 class _OperationScreenState extends State<OperationScreen> {
-  TextEditingController searchController = TextEditingController();
-
   List<OperationModel> operationList = [];
+  List<OperationModel> tempOperationList = [];
   bool isLoading = true;
+
+  // Controller for search text field
+  TextEditingController searchController = TextEditingController();
 
   @override
   void initState() {
@@ -26,9 +29,10 @@ class _OperationScreenState extends State<OperationScreen> {
   }
 
   void _getOperationList() async {
-    operationList.clear();
     final result = await DatabaseService.instance.getOperationInformation();
+    operationList.clear();
     operationList.addAll(result);
+    tempOperationList = operationList;
     isLoading = false;
     setState(() {});
   }
@@ -42,12 +46,15 @@ class _OperationScreenState extends State<OperationScreen> {
   // Function to handle changes in the search text field
   void _onSearchChanged() {
     String query = searchController.text.toLowerCase();
-    // setState(() {
-    //   filteredOperationPackages = availableOperationPackages.where((package) {
-    //     String packageName = package['name'].toLowerCase();
-    //     return packageName.contains(query);
-    //   }).toList();
-    // });
+    if (query.isEmpty) {
+      tempOperationList = operationList;
+    } else {
+      tempOperationList = operationList.where((value) {
+        String name = value.name.toLowerCase();
+        return name.contains(query);
+      }).toList();
+    }
+    setState(() {});
   }
 
   @override
@@ -57,129 +64,110 @@ class _OperationScreenState extends State<OperationScreen> {
         child: isLoading
             ? const Center(child: CircularProgressIndicator())
             : operationList.isEmpty
-                ? const Center(child: Text("List is Empty"))
+                ? const Center(child: EmptyContainerView())
                 : SafeArea(
                     child: SingleChildScrollView(
-                      child: Padding(
-                        padding: const EdgeInsets.all(24),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                IconButton(
-                                  icon: const Icon(
-                                    Icons.arrow_back,
-                                    color: AppColors.primaryColor,
-                                  ),
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
+                      padding: const EdgeInsetsDirectional.only(start: 20, end: 20, bottom: 40),
+                      child: Column(
+                        children: [
+                          const SizedBox(height: 24),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              IconButton(
+                                icon: const Icon(
+                                  Icons.arrow_back,
+                                  color: AppColors.primaryColor,
                                 ),
-                              ],
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            SearchField(
-                              controller: searchController,
-                              onTextChanged: (value) {
-                                setState(() {}); // Trigger rebuild on text change
-                              },
-                            ),
-                            const SizedBox(height: 10),
-                            const Text(
-                              'Available Operations',
-                              style: TextStyle(
-                                fontSize: 24,
-                                fontWeight: FontWeight.bold,
-                                color: AppColors.primaryColor,
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
                               ),
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+                          SearchField(
+                            controller: searchController,
+                            onTextChanged: (value) {},
+                          ),
+                          const SizedBox(height: 10),
+                          const Text(
+                            'Available Operations',
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: AppColors.primaryColor,
                             ),
-                            const SizedBox(height: 40),
-                            ListView.builder(
-                              shrinkWrap: true,
-                              physics: const NeverScrollableScrollPhysics(),
-                              itemCount: operationList.length,
-                              itemBuilder: (BuildContext context, int index) {
-                                final package = operationList[index];
-                                return Column(
+                          ),
+                          const SizedBox(height: 40),
+                          ListView.separated(
+                            shrinkWrap: true,
+                            separatorBuilder: (_, __) => const SizedBox(height: 20),
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: tempOperationList.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              final package = tempOperationList[index];
+                              return Container(
+                                height: 120,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(8),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.grey.withOpacity(0.5),
+                                      spreadRadius: 1,
+                                      blurRadius: 3,
+                                      offset: const Offset(0, 2), // changes position of shadow
+                                    ),
+                                  ],
+                                ),
+                                child: Row(
                                   children: [
-                                    GestureDetector(
-                                      onTap: () {
-                                        // Handle onTap event
-                                      },
-                                      child: Container(
-                                        height: 120,
-                                        decoration: BoxDecoration(
-                                          color: Colors.white,
-                                          borderRadius: BorderRadius.circular(8),
-                                          boxShadow: [
-                                            BoxShadow(
-                                              color: Colors.grey.withOpacity(0.5),
-                                              spreadRadius: 1,
-                                              blurRadius: 3,
-                                              offset: const Offset(0, 2), // changes position of shadow
-                                            ),
-                                          ],
+                                    Container(
+                                      width: 120,
+                                      decoration: BoxDecoration(
+                                        borderRadius: const BorderRadius.only(
+                                          topLeft: Radius.circular(8),
+                                          bottomLeft: Radius.circular(8),
                                         ),
-                                        child: Row(
-                                          children: [
-                                            Container(
-                                              width: 120,
-                                              decoration: BoxDecoration(
-                                                borderRadius: const BorderRadius.only(
-                                                  topLeft: Radius.circular(8),
-                                                  bottomLeft: Radius.circular(8),
-                                                ),
-                                                image: DecorationImage(
-                                                  image: NetworkImage(package.operationImageUrl),
-                                                  fit: BoxFit.cover,
-                                                ),
-                                              ),
-                                            ),
-                                            const SizedBox(width: 16),
-                                            Expanded(
-                                              child: Padding(
-                                                padding: const EdgeInsets.symmetric(vertical: 8),
-                                                child: Column(
-                                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                                  mainAxisAlignment: MainAxisAlignment.center,
-                                                  children: [
-                                                    Text(
-                                                      package.name,
-                                                      style: const TextStyle(fontSize: 18, color: AppColors.primaryColor),
-                                                    ),
-                                                    const SizedBox(
-                                                      height: 5,
-                                                    ),
-                                                    Text(
-                                                      package.description,
-                                                      style: const TextStyle(color: Colors.grey),
-                                                    ),
-                                                    const SizedBox(
-                                                      height: 12,
-                                                    ),
-                                                    Text(
-                                                      'Amount: ${package.amount}',
-                                                      style: const TextStyle(fontWeight: FontWeight.bold, color: Colors.blueGrey),
-                                                    ),
-                                                  ],
-                                                ),
-                                              ),
-                                            ),
-                                          ],
+                                        image: DecorationImage(
+                                          image: NetworkImage(package.operationImageUrl),
+                                          fit: BoxFit.cover,
                                         ),
                                       ),
                                     ),
-                                    const SizedBox(height: 20),
+                                    const SizedBox(width: 16),
+                                    Expanded(
+                                      child: Column(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            package.name,
+                                            maxLines: 2,
+                                            style: const TextStyle(fontSize: 18, color: AppColors.primaryColor),
+                                          ),
+                                          const SizedBox(height: 4),
+                                          Text(
+                                            package.description,
+                                            maxLines: 2,
+                                            overflow: TextOverflow.ellipsis,
+                                            style: const TextStyle(color: Colors.grey),
+                                          ),
+                                          const SizedBox(height: 4),
+                                          Text(
+                                            'Amount: ${package.amount}',
+                                            style: const TextStyle(fontWeight: FontWeight.bold, color: Colors.blueGrey),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
                                   ],
-                                );
-                              },
-                            ),
-                          ],
-                        ),
+                                ),
+                              );
+                            },
+                          ),
+                        ],
                       ),
                     ),
                   ),
